24695f743f20cf783ab256c38ef23d8c9dc619fe
Will <willk2188@gmail.com>
2022-12-08 17:11:51 -0500

    Initial push

---
+++ myapp/main.py
@@ -0,0 +1,18 @@
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel
+
+app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+@app.get("/items/")
+async def read_items(token: str = Depends(oauth2_scheme)):
+    return {"token": token}
