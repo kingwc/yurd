9b6ef465ac8b0e023ce820f3ad12afdab4997961
Will <willk2188@gmail.com>
2022-12-09 00:23:08 -0500

    'others'

--- myapp/src/main.py
+++ myapp/src/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+
+from . import models
+from .database import engine
 from src.APILoginController import APILoginApp
+from src.APIAccountCreateController import api_sign_up_app
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
 app.mount("/login", APILoginApp)
+app.mount('/signup', api_sign_up_app)
